87a0994211af21b6a928e8ce3150f274c0948e21
Ana Victoria <128094441+AVEJA85@users.noreply.github.com>
2024-07-07 21:32:38 -0300

    Actualización de la contraseña en DB

--- src/config.js
+++ src/config.js
@@ -7,7 +7,11 @@ module.exports = {
     mysql: {
         host: process.env.MYSQL_HOST || 'localhost',
         user: process.env.MYSQL_USER || 'root',
+<<<<<<< Updated upstream
         password: process.env.MYSQL_PASSWORD || 'Amelia98',
+=======
+        password: process.env.MYSQL_PASSWORD || 'root',
+>>>>>>> Stashed changes
         database: process.env.MYSQL_DB || 'gamestation',
     }
 }
